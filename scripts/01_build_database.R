e448efe9de7c00e22aebbe9812ce6c7c58b321ef
Darius Bost <42272423+dariusmb85@users.noreply.github.com>
2025-06-05 17:00:58 -0400

    Build new positions parser
    Allows for more robust approach to getting column specs from loc files.

--- scripts/01_build_database.R
+++ scripts/01_build_database.R
@@ -36,43 +36,81 @@ missing_values <- as.character(quote(c(-99, -88, -66, -99.9999999, -88.8888888,
 # FUNCTIONS
 #--------------------------
 
-get_column_positions <- function(year) {
-  if (year == 2021) {
-    readr::fwf_positions(
-      start = c(1, 5, 10, 28, 32, 64, 69, 73, 75, 80),
-      end = c(3, 8, 26, 30, 62, 67, 72, 73, 78, NA)
-    )
-  } else {
-    readr::fwf_positions(
-      start = c(1, 5, 10, 27, 31, 63, 68, 73, 75, 80),
-      end = c(3, 8, 25, 29, 61, 66, 71, 73, 78, NA)
-    )
-  }
+# get_column_positions <- function(year) {
+#   if (year == 2021) {
+#     readr::fwf_positions(
+#       start = c(1, 5, 10, 28, 32, 64, 69, 73, 75, 80),
+#       end = c(3, 8, 26, 30, 62, 67, 72, 73, 78, NA)
+#     )
+#   } else {
+#     readr::fwf_positions(
+#       start = c(1, 5, 10, 27, 31, 63, 68, 73, 75, 80),
+#       end = c(3, 8, 25, 29, 61, 66, 71, 73, 78, NA)
+#     )
+#   }
+# }
+
+get_column_positions <- function(loc_file) {
+  lines <- readLines(loc_file)
+  start_idx <- grep("^\\s*\\d+-\\s*\\d+\\s+", lines)
+  spec_lines <- lines[start_idx]
+  
+  parsed <- lapply(spec_lines, function(line) {
+    matches <- regmatches(line, regexec("^\\s*(\\d+)-\\s*(\\d+)\\s+(.*?)\\s*$", line))[[1]]
+    if (length(matches) >= 4) {
+      list(
+        start = as.integer(matches[2]),
+        end = as.integer(matches[3]),
+        label = trimws(matches[4])
+      )
+    } else {
+      NULL
+    }
+  })
+  
+  parsed <- Filter(Negate(is.null), parsed)
+  colspec <- do.call(rbind, lapply(parsed, as.data.frame))
+  colspec$label <- make.unique(colspec$label)  # Avoid duplicate col names
+  colspec
 }
+
 
 fetch_metadata <- function(data_source, year) {
   lower_source <- paste0(tolower(data_source), "c")
   meta_url <- paste0("https://hcup-us.ahrq.gov/db/state/",
                      lower_source, "/tools/filespecs/", state, "_",
                      data_source, "_", year, "_", data_type, ".loc")
-  positions <- get_column_positions(year)
-  readr::read_fwf(meta_url, positions, skip = 20)
+  positions <- get_column_positions(meta_url)
+  # readr::read_fwf(meta_url, positions, skip = 20)
+  readr::read_fwf(meta_url,
+                  col_positions = fwf_positions(
+                    start = positions$start,
+                    end = positions$end,
+                    col_names = positions$label
+                  ),
+                  skip = 20)
 }
 
 read_hcup_data <- function(meta_df, year) {
-  data_file <- paste0("../", state, "/", data_source, "/", state, "_", data_source, "_",
-                      year, "_", data_type, ".asc")
+  data_file <- paste0(state, "/", data_source, "/", state, "_", data_source,
+                      "_", year, "_", data_type, ".asc")
   readr::read_fwf(
     file = data_file,
-    col_positions = readr::fwf_positions(start = meta_df$X6, end = meta_df$X7, col_names = meta_df$X5),
+    col_positions = readr::fwf_positions(start = meta_df$`Starting column of variable in ASCII file`,
+                                         end = meta_df$`Ending column of variable in ASCII file`,
+                                         col_names = meta_df$`Variable name`),
     skip = 2,
     na = missing_values
   )
 }
 
 write_outputs <- function(df, year) {
-  parquet_path <- here::here("storage", paste0(state, "_", data_source, "_", year, "_", data_type, ".parquet"))
-  duckdb_path <- here::here("storage", paste0(state, "_", data_source, "_", year, "_", data_type, ".duckdb"))
+  parquet_path <- here::here(state, data_source, paste0(state, "_", data_source,
+                                                        "_", year, "_", 
+                                                        data_type, ".parquet"))
+  duckdb_path <- here::here(state, data_source, paste0(state, "_", data_source,
+                                                       "_", year, "_",
+                                                       data_type, ".duckdb"))
   
   # Write Parquet
   df_arrow <- arrow_table(df)
@@ -82,11 +120,7 @@ write_outputs <- function(df, year) {
   con <- dbConnect(duckdb::duckdb(), dbdir = duckdb_path, read_only = FALSE)
   DBI::dbWriteTable(con, "hcup_data", df, overwrite = TRUE)
   dbDisconnect(con, shutdown = TRUE)
-  # con <- dbConnect(duckdb::duckdb(), dbdir = duckdb_path, read_only = FALSE)
-  # arrow::to_duckdb(df, table_name = "temp_arrow_view", con = con)
-  # DBI::dbExecute(con, "CREATE TABLE hcup_data AS SELECT * FROM temp_arrow_view")
-  # dbDisconnect(con, shutdown = TRUE)
-}
+ }
 
 process_hcup <- function(year) {
   message("Processing: ", state, " ", data_source, " ", year)
